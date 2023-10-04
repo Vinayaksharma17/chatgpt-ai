e95f0ef7c1143a514739105d6f1bdbd7cd396c89
Vinayaksharma17 <vvsharma999@gmail.com>
2023-10-04 08:05:29 +0530

    added openia boiler code into generate.js file

--- src/pages/generate.js
+++ src/pages/generate.js
@@ -1,3 +1,13 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// This code is for v4 of the openai package: npmjs.com/package/openai
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [],
+  temperature: 0.8,
+  max_tokens: 256,
+});
