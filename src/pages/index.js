b25e19507e7814d68820f695ad1f19964401573c
Vinayaksharma17 <vvsharma999@gmail.com>
2023-10-04 08:08:43 +0530

    added on change and on submit function into index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,33 @@ import Image from "next/image"
 import favicon from "../../public/favicon.ico"
 
 export default function Home() {
+
+  const [count, setCount] = useState(0)
+  const [animalInput, setAnimalInput] = useState("")
+
+ async function onSubmit(e) {
+      e.preventDefault()
+     try{
+      const response = await fetch("api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({animal: animalInput})
+      })
+      const data = await response.json()
+      if(response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`)
+      }
+
+      setCount( count + 1) 
+      setAnimalInput("")
+     } catch(error) {
+      console.error(error)
+      alert(error.message)
+     }
+  }
+
   return (
     <div>
     <main
@@ -10,12 +37,21 @@ export default function Home() {
     >
      <Image className="w-30 h-30" src={favicon}/>
      <h3 className="text-[green]">Name My Pet</h3>
-     <form>
+     <p>Yoo've used this app {count} times</p>
+     <form onSubmit={onSubmit}>
       <input 
       type='text'
       name='animal'
+      value={animalInput}
+      onChange={(e) => {
+        setAnimalInput(e.target.value)
+        console.log(animalInput);
+      }}
       placeholder="Enter an animal" />
-      <input type="submit" />
+      <input 
+      type="submit" 
+      value="Generate names" />
+
      </form>
       
     </main>
